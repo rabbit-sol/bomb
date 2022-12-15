a1e660d007d35d1408c686177718ad3bcd7add64
SUBHAM KUNWAR <68282784+poisendev91@users.noreply.github.com>
2022-12-15 14:47:21 +0530

    kjg

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -338,6 +338,15 @@ const Boardroom = () => {
 
 
                                                 </Grid>
+
+                                                {!account && (
+                                                <Grid container justify="center" spacing={3} >
+                                                 
+                                                        <UnlockWallet style={{ marginTop: "20px " }}
+                                                            className="shinyButtonSecondary" />
+
+                                                    </Grid>
+                                                )}
                                                 {!!account && (
                                                    
                                                         <Grid container justify="center" spacing={3} >
@@ -353,7 +362,7 @@ const Boardroom = () => {
                                                             >
                                                                 Claim
                                                         </Button>
-                                                    
+                                                       
                                                       
                                                         </Grid>
                                                     
@@ -381,13 +390,146 @@ const Boardroom = () => {
                
               
                
-                    <UnlockWallet />
+                   
                
 
     
             </Box>
 
-     
+            <Box mt={1}>
+
+
+                <Grid container justify="center">
+
+                    <Box mt={1} style={{ width: '100%' }}>
+
+
+
+
+
+
+
+
+
+                        <Card style={{ height: "270px" }} >
+                            <CardContent >
+                               
+                                <Grid container spacing={1}>
+                                    <Grid item >
+                                        <h3>BOMB FARM</h3>
+                                        <h7>Stake your LP tokens in our farm to start earning $BSHARE </h7>
+                                    </Grid>
+                                    
+
+
+
+                                </Grid>
+
+                                <Grid container  spacing={1}>
+                                    <Grid item >
+                                        <Card>
+                                            <CardContent align="center">
+                                                <h5>Daily Return</h5>
+                                                <Typography>{boardroomAPR.toFixed(2)}%</Typography>
+                                            </CardContent>
+                                        </Card>
+                                    </Grid>
+                                    <Grid item  >
+                                        <Card >
+                                            <CardContent align="center">
+                                                <Label text={'Your Stake'} variant="yellow" />
+                                                <Typography>{getDisplayBalance(stakedBalance)}</Typography>
+                                                <Typography>≈ ${tokenPriceInDollars}</Typography>
+
+                                            </CardContent>
+                                        </Card>
+                                    </Grid>
+                                    <Grid item  >
+                                        <Card >
+                                            <CardContent align="center">
+                                                <Label text={'Earned'} variant="yellow" />
+                                                <Typography>{getDisplayBalance(earnings)}</Typography>
+                                                <Typography>≈ ${earnedInDollars}</Typography>
+
+
+
+                                            </CardContent>
+                                        </Card>
+                                    </Grid>
+
+                                    <Grid item  >
+                                        <Card >
+                                            <CardContent align="center">
+                                                <Grid container justify="center" spacing={3} mb={10}>
+                                                    <Button
+                                                        disabled={stakedBalance.eq(0) || (!canClaimReward)}
+                                                        onClick={onRedeem}
+                                                        className={
+                                                            stakedBalance.eq(0) || (!canClaimReward)
+                                                                ? 'shinyButtonDisabledSecondary'
+                                                                : 'shinyButtonSecondary'
+                                                        }
+                                                    >
+                                                        Claim
+                                                    </Button>
+                                                    <Button
+                                                        disabled={stakedBalance.eq(0) || (!canWithdraw)}
+                                                        onClick={onRedeem}
+                                                        mt={4}
+                                                        className={
+                                                            stakedBalance.eq(0) || (!canWithdraw)
+                                                                ? 'shinyButtonSecondary'
+                                                                : 'shinyButtonSecondary'
+                                                        }
+                                                    >
+                                                        Withdraw
+                                                    </Button>
+
+
+                                                </Grid>
+                                                {!!account && (
+
+                                                    <Grid container justify="center" spacing={3} >
+                                                        <Button
+                                                            disabled={stakedBalance.eq(0) || (!canClaimReward)}
+                                                            onClick={onRedeem}
+                                                            style={{ marginTop: "20px " }}
+                                                            className={
+                                                                stakedBalance.eq(0) || (!canClaimReward)
+                                                                    ? 'shinyButtonDisabledSecondary'
+                                                                    : 'shinyButtonSecondary'
+                                                            }
+                                                        >
+                                                            Claim
+                                                        </Button>
+
+
+                                                    </Grid>
+
+                                                )}
+
+                                            </CardContent>
+                                        </Card>
+                                    </Grid>
+
+                                </Grid>
+                            </CardContent>
+                        </Card>
+                    </Box>
+
+                   
+
+
+                </Grid>
+
+
+
+
+               
+
+
+
+            </Box>
         </Page>
     );
 };
